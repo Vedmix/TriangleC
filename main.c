d8d7e60eaa93ac8c3336cfcb5285da8ad9a3e39c
Vedmix <vedmix.work@gmail.com>
2025-01-21 21:32:24 +0300

    0.02 Enter the coordinates

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-	
+	float x1, x2, x3, y1, y2, y3;
+	printf("Enter the coordinates of the points.\n");
+	printf("Point A: ");
+	scanf("%f %f", &x1, &y1);
+	printf("Point B: ");
+	scanf("%f %f", &x2, &y2);
+	printf("Point C: ");
+	scanf("%f %f", &x3, &y3);
 	
 	return 0;
 }
