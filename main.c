dac1a0eb08ba1afa85be6605a85451b286187db8
Vedmix <vedmix.work@gmail.com>
2025-01-21 22:00:12 +0300

    0.03 Sides

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+
+
+float side(float x1, float x2, float y1, float y2){
+	return sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+}
 
 int main(){
-	float x1, x2, x3, y1, y2, y3;
+	float ax, ay, bx, by, cx, cy, ab, bc, ac;
 	printf("Enter the coordinates of the points.\n");
 	printf("Point A: ");
-	scanf("%f %f", &x1, &y1);
+	scanf("%f %f", &ax, &ay);
 	printf("Point B: ");
-	scanf("%f %f", &x2, &y2);
+	scanf("%f %f", &bx, &by);
 	printf("Point C: ");
-	scanf("%f %f", &x3, &y3);
+	scanf("%f %f", &cx, &cy);
+	
+	ab=side(ax, ay, bx, by);
+	bc=side(cx, cy, bx, by);
+	ac=side(ax, ay, cx, cy);
+	
+	printf("Sides %f %f %f", ab, bc, ac);
+	
 	
 	return 0;
 }
