d006cd4747e955a08af8e200d9c494a664a0bb86
Vedmix <vedmix.work@gmail.com>
2025-01-23 19:06:09 +0300

    0.09 Medians of triangle

--- main.c
+++ main.c
@@ -9,14 +9,19 @@ float side(float x1, float y1, float x2, float y2){
 }
 
 float angle(float side1, float side2, float side3){
-	return (acos((pow(side1,2) + pow(side2,2) -pow(side3,2)) / (2 * side1 * side2)) * (180.0 / M_PI));
+	return (acos((pow(side1,2) + pow(side2,2) - pow(side3,2)) / (2 * side1 * side2)) * (180.0 / M_PI));
 	
+}
+
+float median(float side1, float side2, float side3) {
+    return (1.0 / 2.0) * sqrt(2 * pow(side1, 2) + 2 * pow(side2, 2) - pow(side3, 2));
 }
 
 int main(){
 	float ax, ay, bx, by, cx, cy;
 	float ab, bc, ac;
 	float a_angle, b_angle, c_angle;
+	float aa1, bb1, cc1;
 	float p, s;
 	
 	printf("Enter the coordinates of the points.\n");
@@ -42,6 +47,10 @@ int main(){
 	a_angle = angle(ab, ac, bc);
 	b_angle = angle(ab, bc, ac);
 	c_angle = angle(bc, ac, ab);
+	
+	aa1 = median(ac, bc, ab); 
+    bb1 = median(ab, bc, ac);
+    cc1 = median(ab, ac, bc); // медиана к стороне BC
 	
 	if (((ab + bc) > ac) && ((ac + bc) > ab) && ((ab + ac) > bc)){
 	
@@ -91,13 +100,17 @@ int main(){
 	s = sqrt((p / 2) * ((p / 2) - ab) * ((p / 2) - bc) * ((p / 2) - ac));
 	
 	printf("\n");
-	printf("Sides:  AB = %f cm\n", ab);
-	printf("        BC = %f cm\n", bc);
-	printf("        AC = %f cm\n", ac);
+	printf("Sides:   AB = %f cm\n", ab);
+	printf("         BC = %f cm\n", bc);
+	printf("         AC = %f cm\n", ac);
 	printf("\n");
-	printf("Angles: BAC = %f °\n", a_angle);
-	printf("        ABC = %f °\n", b_angle);
-	printf("        BCA = %f °\n", c_angle);
+	printf("Angles:  BAC = %f °\n", a_angle);
+	printf("         ABC = %f °\n", b_angle);
+	printf("         BCA = %f °\n", c_angle);
+	printf("\n");
+	printf("Medians: AA1 = %f cm\n", aa1);
+	printf("         BB1 = %f cm\n", bb1);
+	printf("         CC1 = %f cm\n", cc1);
 	printf("\n");
 	printf("Perimeter: %f cm\n", p);
 	printf("\n");
