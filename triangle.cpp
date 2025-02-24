c6fdc835ffd60efb91466ee2d4cba16f3b56cf7f
Vedmix <vedmix.work@gmail.com>
2025-02-25 00:57:07 +0300

    0.31 Added exist check

--- triangle.cpp
+++ triangle.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+class Triangle{
+private:
+        double ax, ay;
+        double bx, by;
+        double cx, cy;
+public:
+        Triangle(double ax, double ay, double bx, double by, double cx, double cy){
+            if ((ax == bx & ay == by) | (ax == cx & ay == cy) | (cx == bx & cy == by)){
+                cout << "Triangle is not exist!" << "\n";
+                return ;
+            }
+        }
+};
+
 int main(){
     double ax, ay, bx, by, cx, cy;
     cout << "Enter the coordinates of the points" << "\n";
@@ -11,6 +25,6 @@ int main(){
     cin >> bx >> by;
     cout << "Point C: ";
     cin >> cx >> cy;
-
+    Triangle t(ax, bx, cx, ay, by, cy);
     return 0;
 }
