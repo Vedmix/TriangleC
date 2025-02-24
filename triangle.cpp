23a024ee753b98c7d79393ec028f34329a950c42
Vedmix <vedmix.work@gmail.com>
2025-02-24 13:01:54 +0300

    0.31 Added Input

---
+++ triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double ax, ay, bx, by, cx, cy;
+    cout << "Enter the coordinates of the points" << "\n";
+    cout << "Point A: ";
+    cin >> ax >> ay;
+    cout << "Point B: ";
+    cin >> bx >> by;
+    cout << "Point C: ";
+    cin >> cx >> cy;
+
+    return 0;
+}
